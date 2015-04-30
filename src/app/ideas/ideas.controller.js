a890801ff1504403fbc44ef53d0309d01ea7474c
alex <Alex.moinet@yahoo.co.uk>
2015-04-30 10:03:10 +0100

    Changed "name" to title in person.json. Added correct video sourcing to vote and ideas. Ideas.html top person is now dynamically sourced

--- src/app/ideas/ideas.controller.js
+++ src/app/ideas/ideas.controller.js
@@ -2,6 +2,11 @@
 
 angular.module('flare')
   .controller('IdeasCtrl', function ($scope, CommonService) {
+    CommonService.getPerson('1000').then(function(result) {
+      $scope.person = result.data;
+    }, function(result) {
+      console.log(result.status);
+    });
     CommonService.getIdeas().then(function(result) {
       $scope.ideas = result.data;
     }, function(result) {
